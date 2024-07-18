7f5c23224fcff719ebd8eb73faa71637323a318c
fzhnf <faizahnaf52@gmail.com>
2024-07-19 04:02:45 +0800

    refactor: optimize cryptarithm solver and improve code readability
    refactor the loop function from itertools.permutation to custom made
    product, which removes unnecesary loop if the digit for certain
    character is not possible

--- tima/__main__.py
+++ tima/__main__.py
@@ -1,127 +1,124 @@
 #!/usr/bin/env python3
 
-"""
-+------------------------+
-| TUI Cryptarithm Solver |
-+------------------------+
-"""
 
-import itertools
+def iter_product(*iterables, repeat=1):
+    pools = [tuple(pool) for pool in iterables] * repeat
 
-# def product(*iterables, repeat=1):
-#     # https://docs.python.org/3/library/itertools.html#itertools.product
-#     # product('ABCD', 'xy') → Ax Ay Bx By Cx Cy Dx Dy
-#     # product(range(2), repeat=3) → 000 001 010 011 100 101 110 111
-#
-#     pools = [tuple(pool) for pool in iterables] * repeat
-#
-#     result = [[]]
-#     for pool in pools:
-#         result = [x + [y] for x in result for y in pool]
-#
-#     for prod in result:
-#         yield tuple(prod)
+    result = [[]]
+    for pool in pools:
+        result = [x + [y] for x in result for y in pool]
+
+    for prod in result:
+        yield tuple(prod)
 
 
-def is_solution(words, result, mapping, operation):
+def unique_product(*iterables):
+    def inner(i):
+        if i == n:
+            results.append(tuple(result))
+            return
+        for pool in pools[i] - seen:
+            seen.add(pool)
+            result[i] = pool
+            inner(i + 1)
+            seen.remove(pool)
+
+    pools = [set(pool) for pool in iterables]
+    n = len(pools)
+    seen = set()
+    result = [None] * n
+    results = []
+    inner(0)
+    return results
+
+
+def is_solution(words, result, mapping, operation) -> bool:
     words_int = [int("".join(str(mapping[c]) for c in word)) for word in words]
     result_int = int("".join(str(mapping[c]) for c in result))
+    match operation:
+        case "1":
+            return sum(words_int) == result_int
+        case "2":
+            return words_int[0] - sum(words_int[1:]) == result_int
 
-    if operation == "1":  # Addition
-        return sum(words_int) == result_int
-    elif operation == "2":  # Subtraction
-        return words_int[0] - sum(words_int[1:]) == result_int
-    elif operation == "3":  # Multiplication
-        prod = 1
-        for num in words_int:
-            prod *= num
-        return prod == result_int
-    elif operation == "4":  # Division
-        try:
+        case "3":
+            prod = 1
+            for num in words_int:
+                prod *= num
+
+            return prod == result_int
+
+        case "4":
             div = words_int[0]
             for num in words_int[1:]:
+                if num == 0:
+                    return False
                 div /= num
             return div == result_int
-        except ZeroDivisionError:
-            return False
     return False
 
 
-def solve_cryptarithm(words, result, operation):
+def solve_cryptarithm(words, result, operation) -> None:
     unique_chars = set("".join(words) + result)
-    initial_letters = "".join(set(itertools.chain(result[0], (a[0] for a in words))))
+
+    result_initial_letter = result[0]
+    word_initial_letters = [word[0] for word in words]
+    initial_letters = "".join(set([result_initial_letter] + word_initial_letters))
     word_len = [len(word) for word in words]
 
     # leading zeros not allowed by default
     prevent_leading_zero = True
-    result_initial_letter_must_be_one = False
-    if all(x == word_len[0] for x in word_len) and len(result) == word_len[0] + 1:
-        print(f"{result[0]} must be 1")
-        result_initial_letter_must_be_one = True
-
-    # print(initial_letters)
+    prevent_duplicate_character = True
 
     if len(unique_chars) > 10:
         print("Too many unique characters!")
         return
 
     digits = "0123456789"
-    iter_num = 1
-    solution_num = 1
-    for perm in itertools.permutations(digits, len(unique_chars)):
-        mapping = dict(zip(unique_chars, perm))
+    possibility = dict()
 
-        if prevent_leading_zero and any(
-            mapping[letter] == "0" for letter in initial_letters
-        ):
+    for char in unique_chars:
+        if prevent_leading_zero and char in initial_letters:
+            possibility[char] = [*digits.replace("0", "")]
             continue
-        if result_initial_letter_must_be_one and mapping[result[0]] != "1":
-            continue
-        print(mapping[result[0]], end="\r")
+        possibility[char] = [*digits]
 
-        # print(f"iteration-{iter_num}:{mapping}", end="\r")
-        iter_num += 1
-        # for perm in permutations(digits, len(unique_chars)):
-        # print(f"iter:{perm}")
+    if all(x == word_len[0] for x in word_len) and len(result) == word_len[0] + 1:
+        possibility[result[0]] = ["1"]
 
+    print(*possibility.items(), sep="\n")
+
+    keys, values = zip(*possibility.items())
+    n_solution = 1
+    n_iteration = 1
+
+    product = unique_product if prevent_duplicate_character else iter_product
+    for combo in product(*values):
+        mapping = dict(zip(keys, combo))
+        print(f"iter-{n_iteration}:{mapping}", end="\r")
         if is_solution(words, result, mapping, operation):
-            print(f"\n{solution_num} Solution found")
+            print(f"\n solution-{n_solution} found:")
             for word in words:
                 print(f"{word} -> {''.join(mapping[c] for c in word)}")
             print(f"{result} -> {''.join(mapping[c] for c in result)}")
-            solution_num += 1
+            n_solution += 1
+        n_iteration += 1
 
 
 def main():
-    while True:
-        print("Cryptarithm Solver")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Multiplication")
-        print("4. Division")
-        print("5. Exit")
-        choice = input("Choose an operation: ")
+    print("Cryptarithm Solver")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+    choice = input("Choose an operation: ")
 
-        if choice == "5":
-            break
+    words = "II II".upper().split()
+    result = "IUI".upper()
+    print(f"{words} = {result}")
 
-        if choice not in "1234":
-            print("Invalid choice!")
-            continue
-
-        # words = input("Enter the words (space-separated): ").split()
-        # result = input("Enter the result word: ")
-        words = "SEND MORE".upper().split()
-        result = "MONEY".upper()
-        print(f"{words} = {result}")
-
-        solve_cryptarithm(words, result, choice)
-
-        print("\nWould you like to solve another cryptarithm? (yes/no)")
-        continue_choice = input().strip().lower()
-        if continue_choice != "yes":
-            break
-        print("\n" * 3)  # Add empty lines
+    solve_cryptarithm(words, result, choice)
 
 
 if __name__ == "__main__":
